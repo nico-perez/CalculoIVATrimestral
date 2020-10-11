36856b5c76ae8eb9c15326a81d3ce20a6b29d692
nico-perez <0308nperez@e-itaca.es>
2020-10-11 18:24:54 +0200

    El campo pctj de IVA ahora es final

--- src/edu/facturacion/IVA.java
+++ src/edu/facturacion/IVA.java
@@ -5,8 +5,7 @@ public enum IVA
     Normal(.21),
     Reducido(.1),
     Superreducido(.04);
-
-    public double pctj;
+    public final double pctj;
     private IVA(double d)
     {
         pctj = d;
